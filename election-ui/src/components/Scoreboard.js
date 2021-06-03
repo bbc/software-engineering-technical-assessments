66ffe33e4d429d477264f54bd3299b66202a04a8
craigkj <craigkjones@gmail.com>
2021-06-03 13:17:14 +0100

    Add metadata to data results for ui-assessment

--- election-ui/src/components/Scoreboard.js
+++ election-ui/src/components/Scoreboard.js
@@ -8,12 +8,14 @@ function Scoreboard() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [results, setResults] = useState([]);
+  const [isElectionComplete, setIsElectionComplete] = useState(false); 
 
   async function getData() {
     try {
       setLoading(true);
       const resultData = await fetchData();
-      setResults(resultData);
+      setResults(resultData.results);
+      setIsElectionComplete(resultData.isComplete);
       setLoading(false);
     } catch (e) {
       setLoading(false);
--- election-ui/src/dataFetcher/index.js
+++ election-ui/src/dataFetcher/index.js
@@ -3,43 +3,45 @@
 // It maintains a count which is used to increment vote totals.
 // It can be ignored for the purposes of the assessment
 
-const metadata = {
+const defaultData = {
   metadata: {
-    isResult: false
-  }
+    isComplete: false
+  },
+  results: [
+    {
+      'party': 'Adder Party',
+      'candidate': 'Baldrick',
+      'votes': '1056'
+    },
+    {
+      'party': 'Independent',
+      'candidate': 'Lord Buckethead',
+      'votes': '6900'
+    },
+    {
+      'party': 'Independent',
+      'candidate': 'Count Binface',
+      'votes': '9900'
+    }
+  ]
 }
-
-const defaultData = [
-  {
-    'party': 'Adder Party',
-    'candidate': 'Baldrick',
-    'votes': '1056'
-  },
-  {
-    'party': 'Independent',
-    'candidate': 'Lord Buckethead',
-    'votes': '6900'
-  },
-  {
-    'party': 'Independent',
-    'candidate': 'Count Binface',
-    'votes': '9900'
-  }
-];
 
 let callCount = 0;
 const dubiouslyUpdateVoteCount = (item, multiplier = 0) => parseInt(item.votes) + (100 * multiplier);
 const dubiouslyIncrementCount = (count) => count < 6 ? count + 1 : count;
+const dubiouslySetResult = (metadata, count) => count >= 5 ? { ...metadata, isComplete: true } : metadata;
 
 const fetchResults = () => {
   callCount = dubiouslyIncrementCount(callCount);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const apiData = defaultData.map((item) => {
+
+      const metadata = dubiouslySetResult(defaultData.metadata, callCount);
+      const apiResultData = defaultData.results.map((item) => {
         return { ...item, votes: dubiouslyUpdateVoteCount(item, callCount) };
       }, []);
 
-      resolve(apiData);
+      resolve({ ...metadata, results: apiResultData});
     }, 500);
   }
 )};
