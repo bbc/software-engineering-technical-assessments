aa276e4c9cbe4aa436e0d279cf32566c15acda5f
Christopher WInstanley <chris.winstanley@bbc.co.uk>
2022-12-02 09:52:26 +0000

    Fixed the ResultsViewModelTests

--- election-ui-swift/AssessmentSwiftUITests/ResultsViewModelTests.swift
+++ election-ui-swift/AssessmentSwiftUITests/ResultsViewModelTests.swift
@@ -4,9 +4,11 @@ import XCTest
 import Combine
 
 class StubResultsService: ResultsService {
-    var completion: ((Result<ElectionResults.ElectionResponse, ElectionResults.ResultsRepositoryError>) -> Void)?
-    func latestResults(completion:  @escaping (Result<ElectionResults.ElectionResponse, ElectionResults.ResultsRepositoryError>) -> Void) {
-        self.completion = completion
+    var completionResult: (Result<ElectionResponse, ResultsServiceError>, Void)?
+    func latestResults(completion: @escaping (Result<ElectionResponse, ResultsServiceError>) -> Void) {
+        if let result = completionResult {
+            completion(result.0)
+        }
     }
 }
 
@@ -29,7 +31,7 @@ class ResultsViewModelTests: XCTestCase {
         let mockResult = ElectionResult(candidateId: 1, party: "party", votes: 1)
         let mockResults = ElectionResponse(isComplete: false, electionResults: [mockResult])
         
-        stubResultsService.completion!(.success(mockResults))
+        stubResultsService.completionResult = (.success(mockResults), ())
         
         viewModel.refresh()
         
