f5bea88dbf27e21dc90225cb321fe46171b5d7ab
Rossano D'Angelo <rossano.dangelo@bbc.co.uk>
2023-06-02 12:28:24 +0100

    TSGuild-99 set up Jest configuration for TypeScript and copy the test files
    Added Jest configuration for TypeScript support and copied JSON test files from election-api-javascript.

---
+++ election-api-typescript/jest.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
+
+export default config;
