7cd0b76a6bd83aa4d8a1a16cf84a6e7e45e39162
Tom Baxter <thomas99baxter@gmail.com>
2024-06-20 08:57:43 +0100

    reafactor: use mocha instead of jest

--- test/scoreboard.spec.js
+++ test/scoreboard.spec.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const request = require("supertest");
 const { default: expressServer, resetScores } = require("../src/server");
+const { expect } = require("chai");
 
 const resultsSamplesPath = "./test/resources/sample-election-results";
 
@@ -31,19 +32,19 @@ describe("Scoreboard Tests", () => {
     resetScores();
   });
 
-  test("first 5", async () => {
+  it("first 5", async () => {
     await loadResults(server, 5);
     const scoreboard = await fetchScoreboard(server);
-    expect(scoreboard).not.toBeNull();
+    expect(scoreboard).to.not.be.eq(null);
     // assert LD  have won 1 seat
     // assert LAB have won 4 seats
     // assert no winner
   });
 
-  test("first 100", async () => {
+  it("first 100", async () => {
     await loadResults(server, 100);
     const scoreboard = await fetchScoreboard(server);
-    expect(scoreboard).not.toBeNull();
+    expect(scoreboard).to.not.be.eq(null);
     // assert LD  have won 12 seats
     // assert LAB have won 56 seats
     // assert CON have won 31 seats
@@ -53,10 +54,10 @@ describe("Scoreboard Tests", () => {
     // assert SGP have 1071 votes in total
   });
 
-  test("first 554", async () => {
+  it("first 554", async () => {
     await loadResults(server, 554);
     const scoreboard = await fetchScoreboard(server);
-    expect(scoreboard).not.toBeNull();
+    expect(scoreboard).to.not.be.eq(null);
     // assert LD   have won 52  seats
     // assert LAB  have won 325 seats
     // assert CON  have won 167 seats
@@ -66,10 +67,10 @@ describe("Scoreboard Tests", () => {
     // assert IKHH have 18739 votes in total
   });
 
-  test("test all results", async () => {
+  it("test all results", async () => {
     await loadResults(server, 650);
     const scoreboard = await fetchScoreboard(server);
-    expect(scoreboard).not.toBeNull();
+    expect(scoreboard).to.not.be.eq(null);
     // assert LD   have won 62  seats
     // assert LAB  have won 349 seats
     // assert CON  have won 210 seats
