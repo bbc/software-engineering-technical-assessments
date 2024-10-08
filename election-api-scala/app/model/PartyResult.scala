4af33eafd586a31e35fa835c7e44d710ed7e4b40
eleanombre <eleanor.keane@bbc.co.uk>
2024-10-08 11:03:37 +0100

    update TODO comment

--- election-api-scala/app/model/PartyResult.scala
+++ election-api-scala/app/model/PartyResult.scala
@@ -5,7 +5,6 @@ case class ConstituencyResult(id: Int, name: String, seqNo: Int, partyResults: S
 
 case class ApiResponse (error: String, message: String)
 
-// TODO: this class should hold:
-//  - the overall winner (if there is one)
-//  - the seats that each party wins in Parliament
+// TODO:
+// implement this in any way you wish
 case class Scoreboard(winner: String)
